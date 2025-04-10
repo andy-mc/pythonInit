01122e3b06043ed8f9b352acacd8819100634841
Andy Muñoz <quemasandy@gmail.com>
2025-04-10 09:45:05 -0400

    pyhton init

---
+++ hello.py
@@ -0,0 +1,7 @@
+
+name = input("hola como te llamas ? ")
+
+
+print("hello ", name, sep=" - ", end="\n")
+print("hello world ", sep=" - ", end="\n\n\n")
+print("hello world ", sep=" - ", end="\n\n")
